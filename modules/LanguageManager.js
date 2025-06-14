1399c80049763e3d73577d352d0f50073c4c4ecb
maskin00 <sedlerov@gmail.com>
2025-06-14 23:22:42 +0300

    1.71 Ё и Ь

--- modules/LanguageManager.js
+++ modules/LanguageManager.js
@@ -489,12 +489,12 @@ class LanguageManager {
                     keyboard.slice(32)       // Э Ё - пробел
                 ].filter(row => row.length > 0);
             } else if (layout === 'colemak') {
-                // Русская Colemak: 10 + 10 + 12 + 3 (равномерное распределение)
+                // Русская Colemak: 10 + 10 + 11 + 4 (оптимизированное распределение)
                 return [
                     keyboard.slice(0, 10),   // Й В Ф П Г Ж Л У Ы Ъ
                     keyboard.slice(10, 20),  // А Р С Т Д Х Н Е И О
-                    keyboard.slice(20, 32),  // Я Ч Ц М Б К Ш Щ З Э Ю Ь
-                    keyboard.slice(32)       // Ё - пробел
+                    keyboard.slice(20, 31),  // Я Ч Ц М Б К Ш Щ З Э Ю
+                    keyboard.slice(31)       // Ё - пробел Ь
                 ].filter(row => row.length > 0);
             } else {
                 // Русская QWERTY: 12 + 11 + 10 + 2 (стандартная раскладка)
